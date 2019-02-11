c899ad44bb6fe4a548afa9938f291432c6099154
annasmith206 <36894791+annasmith206@users.noreply.github.com>
2019-02-10 16:42:50 -0800

    did some stuff

--- assignment02/Hangman.java
+++ assignment02/Hangman.java
@@ -1,13 +1,18 @@
 import java.util.Arrays;
 import javax.swing.JOptionPane;
 
+
 public class Hangman
 {
     public static void main(String args[])
     {
-        StartGame();
+        //showMenu();
+        //StartGame();
+        char[] guessWord = {'_', 'a', '_', '_'};
+        int numGuesses = 1;
+        showProgress(guessWord, numGuesses);
     }
-
+/*
     public static void StartGame()
     {
         switch(showMenu())
@@ -28,25 +33,29 @@ public class Hangman
 
     public static void RunGame(String word)
     {
-        char[] guessedWord = new char[word.Length() * 2];
-        Array.fill(guessedWord, '_');
+        char[] guessedWord = new char[word.length() * 2];
+        Arrays.fill(guessedWord, '_');
 
-        Boolean isGuessed;
-        int strikes;
+        Boolean isGuessed = false;
+        int strikes = 0;
         while(strikes <= 5 || !isGuessed)
         {
 
         }
     }
+*/
 
     public static int showMenu()
     {
-
+      String strOption = JOptionPane.showInputDialog("Enter 1 to chose a random word\nEnter 2 to chose a word from the user\nEnter 3 to exit game");
+      int option = Integer.parseInt(strOption) - 1;
+      System.out.println(option);
+      return option;
     }
-
+/*
     public static String enterWord()
     {
-        
+
     }
 
     public static String getRandomWord()
@@ -56,14 +65,18 @@ public class Hangman
 
     public static Boolean TryLetter(char[] guessWord, char guessLetter, String word)
     {
-        
+
     }
+*/
+
 
     public static void showProgress(char[] guessWord, int numGuesses)
     {
-
+      if (numGuesses != 1){
+        JOptionPane.showMessageDialog(null, new String(guessWord) + "\nYou have " + numGuesses + " guesses left.");
+      }else{
+        JOptionPane.showMessageDialog(null, new String(guessWord) + "\nYou have " + numGuesses + " guess left.");
+      }
     }
-
-
 
 }
