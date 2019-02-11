48fba35ab892182f40f7f6ba037939493f6fd3a1
Ben Howard <thebenjhoward@gmail.com>
2019-02-10 16:54:44 -0800

    some more stuff

--- assignment02/Hangman.java
+++ assignment02/Hangman.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import javax.swing.JOptionPane;
+import java.util.Random;
 
 public class Hangman
 {
@@ -28,11 +29,11 @@ public class Hangman
 
     public static void RunGame(String word)
     {
-        char[] guessedWord = new char[word.Length() * 2];
-        Array.fill(guessedWord, '_');
+        char[] guessedWord = new char[word.length() * 2];
+        Arrays.fill(guessedWord, '-');
 
-        Boolean isGuessed;
-        int strikes;
+        Boolean isGuessed = false;
+        int strikes = 0;
         while(strikes <= 5 || !isGuessed)
         {
 
@@ -46,17 +47,51 @@ public class Hangman
 
     public static String enterWord()
     {
-        
+        return JOptionPane.showInputDialog("Enter your desired word");
     }
 
     public static String getRandomWord()
     {
+        String[] wordList = { "antidisestablishmentarianism", "no", "bubbles", 
+        "sunshine", "help", "happiness", "I am being held against my will",
+        "supercalifragilisticexpialidocious", "fear", "anger", "hate", "suffering",
+        "pneumonoultramicroscopicsilicovolcanoconiosis", "the quick brown fox jumped "
+        + "over the lazy black dog" };
 
+        Random rand = new Random();
+        return wordList[rand.nextInt(wordList.length)];
     }
 
     public static Boolean TryLetter(char[] guessWord, char guessLetter, String word)
     {
-        
+        Boolean success = false;
+        Boolean alreadyEntered = false;
+        for(int i = 0; i < word.length(); i++)
+        {
+            if(guessWord[i] == '-' && word.charAt(i) == guessLetter)
+            {
+                success = true;
+                guessWord[i] = guessLetter;
+            }
+            else if(guessWord[i] == guessLetter)
+            {
+                alreadyEntered = true;
+            }
+        }
+
+        if(success)
+        {
+            JOptionPane.showMessageDialog(null, "You guessed a letter!");            
+        }
+        else if(alreadyEntered)
+        {
+            JOptionPane.showMessageDialog(null, "You already guessed that letter!");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Sorry, that is incorrect.");
+        }
+        return success || alreadyentered;
     }
 
     public static void showProgress(char[] guessWord, int numGuesses)
@@ -64,6 +99,15 @@ public class Hangman
 
     }
 
-
+    public static void addSpaces(char[] guessWord, String word)
+    {
+        for(int i = 0; i < word.length(); i++)
+        {
+            if(word.charAt(i) == ' ')
+            {
+                guessWord[i] = ' ';
+            }
+        }
+    }
 
 }
