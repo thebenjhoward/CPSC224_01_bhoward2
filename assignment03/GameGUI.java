b6766b533cbf586fa65908f7f6420b3a55597c17
annasmith206 <36894791+annasmith206@users.noreply.github.com>
2019-02-24 17:05:44 -0800

    added statusPanel

--- assignment03/GameGUI.java
+++ assignment03/GameGUI.java
@@ -2,7 +2,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GameGUI extends JFrame {
+public class GameGUI extends JFrame
+{
     private PlayerPanel player1, player2;
     private StatusPanel status;
     private BoardPanel board;
@@ -11,53 +12,54 @@ public class GameGUI extends JFrame {
     private JButton exitButton;
     private JPanel players;
     private JPanel buttonPanel;
-    private final int WINDOW_WIDTH = 500;
-    private final int WINDOW_HEIGHT = 500;
+    private final int WINDOW_WIDTH = 400;
+    private final int WINDOW_HEIGHT = 100;
 
-    public GameGUI() {
-        // Display a title
-        setTitle("Tic Tac Toe");
-        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    public GameGUI()
+    {
+      // Display a title
+      setTitle("Tic Tac Toe");
 
-        // Specify an action for the close button.
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      // Specify an action for the close button.
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create a BorderLayout manager.
-        setLayout(new BorderLayout());
-        buildPlayerPanel();
-        buildButtonPanel();
-        add(players, BorderLayout.NORTH);
-        add(buttonPanel, BorderLayout.SOUTH);
+      // Create a BorderLayout manager.
+      setLayout(new BorderLayout());
+      buildPlayerPanel();
+      buildButtonPanel();
+      add(players, BorderLayout.NORTH);
+      add(buttonPanel, BorderLayout.SOUTH);
 
-        // Create custom panels
+      // Create custom panels
 
-        // status = new StatusPanel();
-        board = new BoardPanel(this);
-        add(board, BorderLayout.CENTER);
+      //status = new StatusPanel();
+      //board = new BoardPanel();
 
-        // Create the button panel
-        // buildButtonPanel();
+      //Create the button panel
+      //buildButtonPanel();
 
-        // add(board);
-        // add(buttonPanel);
-        // add(status);
 
-        // pack();
-        setVisible(true);
-    }
+    //add(board);
+      //add(buttonPanel);
+      //add(status);
 
-    private void buildPlayerPanel() {
-        players = new JPanel();
-        // players.setSize(500, 500);
-        players.setLayout(new GridLayout(1, 2));
-        player1 = new PlayerPanel(1);
-        player2 = new PlayerPanel(2);
-        players.add(player1);
-        players.add(player2);
-    }
+      pack();
+      setVisible(true);
+      }
+
+
+      private void buildPlayerPanel(){
+          players = new JPanel();
+          players.setSize(500, 500);
+          player1 = new PlayerPanel(1);
+          player2 = new PlayerPanel(2);
+          players.add(player1);
+          players.add(player2);
+      }
 
     // buildButton panel method that builds the button panel
-    private void buildButtonPanel() {
+      private void buildButtonPanel()
+      {
         // Create a panel for the buttons.
         buttonPanel = new JPanel();
 
@@ -75,67 +77,47 @@ public class GameGUI extends JFrame {
         buttonPanel.add(newGameButton);
         buttonPanel.add(resetButton);
         buttonPanel.add(exitButton);
-    }
+      }
 
-    public void NextTurn() {
-        
-        // Add change to status panel here
-        
-        // Check for winner 
-        BoardPanel.WinType winStatus = board.getWinStatus();
-        switch (winStatus) {
-        case X_WIN:
-            JOptionPane.showMessageDialog(this, player1.getName() + " Wins!");
-            player1.addWin();
-            player2.addLoss();
-            board.disableButtons();
-            // Add change to status panel here
-            break;
-        case O_WIN:
-            JOptionPane.showMessageDialog(this, player1.getName() + " Wins!");
-            player1.addWin();
-            player2.addLoss();
-            board.disableButtons();
-            break;
-        default:
-            break;
-        }
-    }
 
-    private class NewGameButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
+      private class NewGameButtonListener implements ActionListener
+      {
+        public void actionPerformed(ActionEvent e)
+        {
             System.out.println("new game button");
             player1.startGame();
             player2.startGame();
-
-            board.enableButtons();
             /*
-             * if game is in progress, call reset functions and then enable functions, else
-             * just call enable functions
-             */
-
-            // Add change to status panel here
+            if game is in progress, call reset functions and then enable functions,
+            else just call enable functions
+            */
 
         }
-    }
+      }
 
-    private class ResetButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
+
+      private class ResetButtonListener implements ActionListener
+      {
+        public void actionPerformed(ActionEvent e)
+        {
             System.out.println("reset button!");
             /*
-             * calls reset functions
-             */
+            calls reset functions
+            */
         }
-    }
+      }
 
-    private class ExitButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            System.exit(0);
+      private class ExitButtonListener implements ActionListener
+      {
+        public void actionPerformed(ActionEvent e)
+        {
+          System.exit(0);
         }
-    }
+      }
 
-    public static void main(String[] args) {
+      public static void main(String[] args)
+      {
         new GameGUI();
-    }
+      }
 
 }
--- assignment03/StatusPanel.java
+++ assignment03/StatusPanel.java
@@ -2,8 +2,22 @@ import javax.swing.*;
 
 public class StatusPanel extends JPanel
 {
+    private JLabel status;
+
     public StatusPanel()
     {
+      status = new JLabel("Press New Game to Begin.");
+      this.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+      add(greeting);
+    }
 
+    public void setText(String text)
+    {
+      status.setText(text);
+    }
+
+    public void reset()
+    {
+      status.setText("Press New Game to Begin.");
     }
 }
