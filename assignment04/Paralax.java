0c7f5f34559d7d52e586728a10788ac14bfaa9a8
annasmith206 <36894791+annasmith206@users.noreply.github.com>
2019-03-24 15:12:59 -0700

    added main and timer in ParalaxPanel

--- assignment04/Paralax.java
+++ assignment04/Paralax.java
@@ -6,5 +6,14 @@ import javax.swing.JFrame;
 import javax.swing.Timer;
 
 public class Paralax {
+    public static void main(String args[])
+    {
+      JFrame frame = new JFrame("Paralax");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+      ParalaxPanel pPanel = new ParalaxPanel();
+      frame.add(pPanel);
+      frame.setSize(300, 300);
+      frame.setVisible(true);
+    }
 }
--- assignment04/ParalaxPanel.java
+++ assignment04/ParalaxPanel.java
@@ -1,6 +1,25 @@
 import javax.swing.*;
 import javax.swing.JPanel;
+import java.util.*;
+import javax.swing.Timer;
+import java.awt.event.*;
 
-public class ParalaxPanel extends JPanel {
-    
+public class ParalaxPanel extends JPanel implements ActionListener{
+    private int delay = 10;
+    protected Timer timer;
+
+    private ArrayList<PXObject> pxObjects = new ArrayList<PXObject>();
+
+    public ParalaxPanel()
+    {
+      timer = new Timer(delay, this);
+      timer.start(); // starts timer
+    }
+
+    // runs when the timer fires
+    public void actionPerformed(ActionEvent e)
+    {
+      System.out.println("TIMER");
+      repaint();
+    }
 }
