9560fc0bcfc8c7e3897a3bcfa3a82a86bcd2340d
annasmith206 <36894791+annasmith206@users.noreply.github.com>
2019-04-23 19:31:14 -0700

    set up story panel class

--- project/StoryPlayer/StoryPanel.java
+++ project/StoryPlayer/StoryPanel.java
@@ -1,9 +1,38 @@
 package StoryPlayer;
 
+import StoryData.StoryNode;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class StoryPanel extends JPanel {
+  private StoryNode currentNode;
+  private ChoicesPanel choices;
+  private InfoPanel info;
+
+  public StoryPanel(){
+    initializeGUI();
+    setNode(null);
+    
+  }
+
+  public StoryPanel(StoryNode rootNode){
+      initializeGUI();
+      setNode(rootNode);
+  }
+
+  /*---------------------------------------------------------
+  //////////////////// HELPER FUNCTIONS /////////////////////
+  -----------------------------------------------------------*/
+
+  // uses set methods to update GUI
+  public void setNode(StoryNode node){
+    currentNode = node;
+  }
+
+  // add components
+  public void initializeGUI(){
+
+  }
 
 }
