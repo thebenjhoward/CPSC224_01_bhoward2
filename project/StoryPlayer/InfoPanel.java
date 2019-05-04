e5a7704e85a2dd266fa2322cab95a99c2daf2e11
annasmith206 <36894791+annasmith206@users.noreply.github.com>
2019-05-04 16:35:27 -0700

    worked on playerMenuPanel

--- project/StoryPlayer/InfoPanel.java
+++ project/StoryPlayer/InfoPanel.java
@@ -9,8 +9,6 @@ import javax.imageio.*;
 
 public class InfoPanel extends JPanel {
     private JTextArea storyTextArea;
-    private JLabel imageLabel;
-    private Image tempImage; // remove later
 
     public InfoPanel() {
         // set up panel layout
@@ -23,36 +21,16 @@ public class InfoPanel extends JPanel {
         storyTextArea.setEditable(false);
         JScrollPane storyScrollPane = new JScrollPane(storyTextArea);
         storyScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        storyScrollPane.setPreferredSize(new Dimension(530, 200));
+        storyScrollPane.setPreferredSize(new Dimension(750, 200));
         storyScrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));
-
-        // set up image label
-        loadImage();
-        imageLabel = new JLabel(new ImageIcon(tempImage));
-        imageLabel.setPreferredSize(new Dimension(180, 180));
-        imageLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         // add stuff
         this.add(storyScrollPane);
-        this.add(imageLabel);
         this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         this.setPreferredSize(new Dimension(750, 200));
-    }
-
-    private void loadImage() {
-        try {
-            tempImage = ImageIO.read(new File("StoryPlayer\\PlaceHolderImage.png"));
-        } catch (IOException e) {
-            System.out.println(e.toString());
-        }
     }
 
     public void setText(String text) {
         storyTextArea.setText(text);
         storyTextArea.setEnabled(!(text == null || text.equals("")));
     }
-
-    public void setImage(Image image) {
-        imageLabel.setIcon(new ImageIcon(image));
-    }
-}
--- project/StoryPlayer/PlayerMenuPanel.java
+++ project/StoryPlayer/PlayerMenuPanel.java
@@ -6,5 +6,55 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class PlayerMenuPanel extends JPanel {
-    
+  private JFileChooser fileChooser;
+  private JMenuBar menuBar;
+  private JMenu fileMenu;
+  private JMenuItem open, exit;
 }
+
+  public PlayerMenuPanel() {
+    JFileChooser fileChooser = new JFileChooser();
+    fileChooser.setFileFilter(new FileNameExtensionFilter("XML Files", "xml"));
+    int result = fileChooser.showOpenDialog(this.getRootPane());
+    this.setLayout(new GridLayout(1, 1));
+
+    menuBar = new JMenuBar();
+    fileMenu = new JMenu("File");
+    open = new JMenuItem("Open");
+    exit = new JMenuItem("Exit");
+
+    open.addActionListener(this);
+    exit.addActionListener(this);
+
+    fileMenu.add(open);
+    fileMenu.addSeparator();
+    fileMenu.add(exit);
+
+    menuBar.add(fileMenu);
+    this.add(menuBar);
+
+  }
+}
+
+
+
+
+  /**
+   * Sets the Global Event Listener. This listener will relay events to the
+   * {@code StoryDesigner} panel
+   *
+   * @param l The listener to be added
+   */
+  public void setGlobalEventListener(ActionListener l) {
+      globalEventListener = l;
+  }
+
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == open) {
+
+    } else if (e.getSource() == exit) {
+
+    }
+  }
